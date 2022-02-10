04ecbbdb179d51957db17a7d193ff7e0cfc42545
Strom <stromborman@gmail.com>
2022-02-09 18:43:44 -0600

    implemented little-step big-step to solve 2020:25 in O(sqrt(n)) time

--- advent2020/day25.py
+++ advent2020/day25.py
@@ -6,7 +6,7 @@ https://adventofcode.com/2020/day/25
 
 This problem is an instance of Diffie–Hellman key exchange.
 
-Alice and Bob publiclly agree on prime p and number g (with order p-1 in Z_p*)
+Alice and Bob publiclly agree on prime p and number g (with order p-1 in (Z/p)^*)
 (in this problem p = 20201227 and g = 7)
 
 Alice and Bob secretely each pick private A and B and
@@ -16,45 +16,59 @@ publically transmit a = g^A mod p and b = g^B mod p
 Alice and Bob then compute: F = b^A = a^B mod p (since g^AB = g^BA)
 (in this problem F is the answer)
 
-A brief google search suggests there is no known clever way to recover F
-from the public information of p, g, a, b.  The solution below
-just tries each n until it finds g^n = a.
-"""
+Below we implement the baby-step giant-step algorithm for solving
+the discrete logorithm problem: given g^A mod p, recover A
 
+With the private A we compute F = b^A mod p
+"""
+import numpy as np
+  
+def gcdE(a, b): # output solves gcd=ax+by 
+    if a == 0 :  
+        return b,0,1
+             
+    gcd,x1,y1 = gcdE(b%a, a) 
+    
+    x = y1 - (b//a) * x1 
+    y = x1 
+     
+    return gcd,x,y    
+
+
+def inverse(a,n): # computes a^-1 in (Z/n)^*
+    return gcdE(a,n)[1]
+
+
+def stepSTEP(n:int, g:int, a:int) -> int: # where g^output = a mod n  
+    # solving g^x = a for x=im+j
+    # equivalent to solving g^j = a*(g^-m)^i for (i,j)
+    # we compute g^-m via gcd (called s below) and each possible g^j
+    # then brute force search of an i that works
+    
+    m = int(np.ceil(np.sqrt(n)))
+    lookup = {0:1}
+    while len(lookup) <= m:
+        j = len(lookup)
+        lookup[j] = ((lookup[j-1]*g) %n)
+    
+    s = gcdE(lookup[m], n)[1]
+    
+    for i in range(m):
+        #print('big step', i*m)
+        for j in range(m):
+            if lookup[j] == (a*(s**i)) % n:
+                return i*m + j
+    
+        
+def power(x,b,n): # computes x^b mod n
+    out = 1
+    for i in range(b):
+        out = (out*x) % n
+    return out
+    
 test = [5764801, 17807724]
 real = [10212254, 12577395]
-
 p = 20201227
-c_pub, d_pub = real[0], real[1]
 
-# we know x_pub = 7**x_pri mod p
-# we need to solve for either x_pri
-
-# brute force
-def pub_to_pri(pub0,pub1): # c_pub -> 1063911
-    looking = True
-    n = 0
-    last = 1
-    while looking and n < p:
-        if last % p == pub0:
-            looking = False
-            return (0, n)
-        elif last % p == pub1:
-            looking = False
-            return(1, n)
-        else:
-            last = last*7
-            n = n+1
-
-k_pri = 1063911
-
-# the final answer is then d_pub**c_pri mod p
-def trans(x,n):
-    out = 1
-    for i in range(n):
-        out = (out*x) % p
-    return out
-
-card_secret = 1063911
-print('Answer:')
-print(trans(12577395, 1063911))
+print('Answer:')    
+print(power(real[1], stepSTEP(p, 7, real[0]), p))    
