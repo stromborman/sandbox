87ed05201212c4b84bdecc18073151063639c7b1
Strom <stromborman@gmail.com>
2022-06-08 18:49:23 -0500

    Finished medium problems

--- other/neetcode/linked_list.py
+++ other/neetcode/linked_list.py
@@ -162,10 +162,92 @@ def removeNthFromEnd(head: Optional[ListNode], n: int) -> Optional[ListNode]:
         stack[-n-1].next = target
 
         return head
+
+"""
+Copy List with Random Pointer:
+    A linked list of length n is given such that each node contains an 
+    additional random pointer, which could point to any node in the list, or null.
+    Construct a deep copy of the list.
+"""
+
+class NodeR:
+    def __init__(self, val=0, next=None, random=None):
+        self.val = val
+        self.next = next
+        self.random = random
+
+
+def copyRandomList(head: 'Optional[NodeR]') -> 'Optional[NodeR]':
+    
+    if head == None:
+        return None
+    
+    def toDictAndList(head):
+        dic = {}
+        i = 0
+        while head:
+            dic[head] = i
+            i= i+1
+            head=head.next
+        dic[None] = i
+        lst = [node for node in dic.keys()]
+        return dic, lst
+    
+    dic, lst = toDictAndList(head)
+    
+    head_copy = NodeR(head.val)
+    lst_copy = [head_copy]
+    work = head_copy
+    for node in lst[1:-1]:
+        new = NodeR(node.val)
+        lst_copy.append(new)
+        work.next = new
+        work = new
+    lst_copy.append(None)
+    
+    for i,node in enumerate(lst_copy[:-1]):
+        node.random = lst_copy[dic[lst[i].random]]
+        
+    return head_copy
+
+
+
+"""
+Add Two Numbers:
+    You are given two non-empty linked lists representing two non-negative integers. 
+    The digits are stored in reverse order, and each of their nodes contains a single digit. 
+    Add the two numbers and return the sum as a linked list.
+"""
+
+def addTwoNumbers(l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+    
+    def getNum(linked):
+        num = ''
+        while linked:
+            num = num + str(linked.val)
+            linked = linked.next
+        return int(num[::-1])
+    
+    def getLinked(num):
+        lst = list(str(num))
+        head = ListNode(int(lst.pop()))
+        work = head
+        while lst:
+            new = ListNode(int(lst.pop()))
+            work.next = new
+            work = new
+        return head
+    
+    return getLinked(getNum(l1) + getNum(l2))
+
+
     
 """
 Linked List Cycle:
     Determine if there is a cycle. Told at most 10^4 nodes.
+    This problem was meant to be solved with Floyd's algorithm in the 
+    duplicate number solution below.  Was able to brute force O(n)
+    time this since we were given n beforehand.
 """
 
 def hasCycle(head: Optional[ListNode]) -> bool:
@@ -179,4 +261,113 @@ def hasCycle(head: Optional[ListNode]) -> bool:
             return True
     return False
 
-   
+
+
+
+"""
+Find the Duplicate Number:
+    Given an array of integers nums of length n+1 where each 
+    integer is in the range [1, n] inclusive and exactly one number
+    appears >= 2 times.  Find that number.
+    
+    View the array as a linked list where Node(k).next = Node(nums[k]) for k in 0,1,..,n
+    The duplicated number with be the first number that repeats in the linked list.
+    No need to actually make the linked list, since nums has the equivalent information.
+    
+    Floyd's algorithm can detect this number with O(1) extra memory and O(n) time.
+    Naive solutions are memory*time = O(n^2). 
+"""
+
+def findDuplicate(nums: List[int]) -> int:
+    
+    fast, slow = 0, 0
+    flag = True
+    while flag:
+        fast = nums[nums[fast]]
+        slow = nums[slow]
+        if fast == slow:
+            flag=False
+    slow = 0
+    while slow != fast:
+        slow = nums[slow]
+        fast = nums[fast]
+    
+    return slow   
+
+
+
+
+"""
+LRU Cache:
+
+    Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
+    
+    Implement the LRUCache class:
+    
+        LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
+        int get(int key) Return the value of the key if the key exists, otherwise return -1.
+        void put(int key, int value) Update the value of the key if the key exists. 
+        Otherwise, add the key-value pair to the cache. If the number of keys exceeds the 
+        capacity from this operation, evict the least recently used key.
+    
+    The functions get and put must each run in O(1) average time complexity.
+"""
+
+class Node:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.dic = {} # will map key to node(key,val)
+        
+        # These will never be added to the dic, so their key/values do not matter
+        self.left = Node(0,0)
+        self.right = Node(0,0)
+        
+        self.left.next = self.right
+        self.right.prev = self.left
+        
+    def remove(self,node):
+        # The nodes to left/right of node
+        pre, nex = node.prev, node.next
+        # connect them together
+        pre.next, nex.prev = nex, pre      
+    
+    def insert(self,node):
+        # the nodes at the end
+        pre, nex = self.right.prev, self.right
+        
+        # insert node between them
+        node.prev, node.next = pre, nex
+        pre.next = nex.prev = node
+        
+        self.dic[node.key] = node
+        
+    
+    def get(self, key: int) -> int:
+        if key in self.dic.keys():
+            self.remove(self.dic[key])
+            self.insert(self.dic[key])
+            return self.dic[key].val
+        return -1     
+        
+    def put(self, key: int, value: int) -> None:
+        if key in self.dic.keys():
+            self.remove(self.dic[key])
+        
+        # make new node and add it
+        self.dic[key] = Node(key,value)
+        self.insert(self.dic[key])
+        
+        # clear cache
+        if len(self.dic) > self.cap:
+            lru = self.left.next
+            self.remove(lru)
+            del self.dic[lru.key]
+
